f249f5e07b200619f5d29c75eb9a8fd862cc71c2
Will <willpatera@gmail.com>
2018-03-29 13:56:53 +0700

    add logging to bundle shell and specify python version for bundle scripts

--- deployment/bundle.sh
+++ deployment/bundle.sh
@@ -8,20 +8,24 @@ echo "release_dir:  ${release_dir}"
 mkdir ${release_dir}
 
 # build dependencies
+echo -e "\n##########\nBuilding dependencies\n##########\n"
 python3 ../pupil_src/shared_modules/pupil_detectors/build.py
 python3 ../pupil_src/shared_modules/cython_methods/build.py
 
 # bundle Pupil Capture
+echo -e "\n##########\nBundling Pupil Capture\n##########\n"
 cd deploy_capture
 ./bundle.sh
 mv *.deb ../$release_dir
 
 # bundle Pupil Service
+echo -e "\n##########\nBundling Pupil Service\n##########\n"
 cd ../deploy_service
 ./bundle.sh
 mv *.deb ../$release_dir
 
 # bundle Pupil Player
+echo -e "\n##########\nBundling Pupil Player\n##########\n"
 cd ../deploy_player
 ./bundle.sh
 mv *.deb ../$release_dir
