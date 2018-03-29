f43f13b23ea720b81ab9af61298496aa9729d0ad
Will <willpatera@gmail.com>
2018-03-29 14:35:33 +0700

    test release upload from travis --> github

--- deployment/bundle.sh
+++ deployment/bundle.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Note: this script assumes that it is being run from within the deploymnet dir
+cd pupil/deployment
 
 # create release dir from latest tag
 current_tag=$(git describe --tags | awk -F"-" '{print $1"."$2}')
@@ -8,27 +8,28 @@ echo "release_dir:  ${release_dir}"
 mkdir ${release_dir}
 
 # build dependencies
-echo -e "\n##########\nBuilding dependencies\n##########\n"
+printf "\n##########\nBuilding dependencies\n##########\n"
 python3 ../pupil_src/shared_modules/pupil_detectors/build.py
 python3 ../pupil_src/shared_modules/cython_methods/build.py
 
 # bundle Pupil Capture
-echo -e "\n##########\nBundling Pupil Capture\n##########\n"
+printf "\n##########\nBundling Pupil Capture\n##########\n\n"
 cd deploy_capture
 ./bundle.sh
 mv *.deb ../$release_dir
 
 # bundle Pupil Service
-echo -e "\n##########\nBundling Pupil Service\n##########\n"
+printf "\n##########\nBundling Pupil Service\n##########\n\n"
 cd ../deploy_service
 ./bundle.sh
 mv *.deb ../$release_dir
 
 # bundle Pupil Player
-echo -e "\n##########\nBundling Pupil Player\n##########\n"
+printf "\n##########\nBundling Pupil Player\n##########\n\n"
 cd ../deploy_player
 ./bundle.sh
 mv *.deb ../$release_dir
 
-# temporary line for diagnostics before we gzip and transfer bundles
-cd ../ && ls -alR
+cd ..
+printf "\n##########\nzipping release\n##########\n\n"
+zip -r $release_dir.zip $release_dir
